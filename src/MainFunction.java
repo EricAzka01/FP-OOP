910f557d77605a2294a6abb8065c8770b5726063
Eric Azka Nugroho <eric.a.nugroho@gmail.com>
2022-12-03 15:32:41 +0700

    hello test

--- src/MainFunction.java
+++ src/MainFunction.java
@@ -6,6 +6,7 @@ public class MainFunction {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
 
     }
 }
