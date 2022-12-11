ffc36574e550ae6c688183e5813da40b9aea322d
Eric Azka Nugroho <eric.a.nugroho@gmail.com>
2022-12-11 16:13:30 +0700

    Progress: Adding Calculation

--- src/Form.java
+++ src/Form.java
@@ -1,16 +1,16 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class Form extends JFrame{
     private JPanel panel1;
     private JPanel welcome;
-    private JCheckBox burgerCheckBox;
-    private JCheckBox pizzaCheckBox;
-    private JCheckBox iceTeaCheckBox;
-    private JCheckBox mineralWaterCheckBox;
-    private JLabel Price;
-    private JLabel pricedrink;
+    private JLabel Apps;
+    private JButton resetButton;
+    private JButton calculateButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
     private JLabel Welcome;
 
     public Form(){
